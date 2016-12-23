2292d4ecd86f887249212bbb361eaeababd1f69e
Bubb, Matt <mdbubb@users.noreply.github.com>
2016-12-23 10:31:19 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,122 +4,142 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void printBoard(String[][] board) {
+   public static void printBoard(String[][] board) {
 
-        System.out.print(board[0][0] + " | ");
-        System.out.print(board[0][1] + " | ");
-        System.out.println(board[0][2] + " | ");
-        System.out.print(board[1][0] + " | ");
-        System.out.print(board[1][1] + " | ");
-        System.out.println(board[1][2] + " | ");
-        System.out.print(board[2][0] + " | ");
-        System.out.print(board[2][1] + " | ");
-        System.out.print(board[2][2] + " |  ");
-        System.out.println();
-    }
+       System.out.print(board[0][0] + " | ");
+       System.out.print(board[0][1] + " | ");
+       System.out.println(board[0][2] + " | ");
+       System.out.print(board[1][0] + " | ");
+       System.out.print(board[1][1] + " | ");
+       System.out.println(board[1][2] + " | ");
+       System.out.print(board[2][0] + " | ");
+       System.out.print(board[2][1] + " | ");
+       System.out.print(board[2][2] + " |  ");
+       System.out.println();
+   }
+   public static boolean placement(String[][] board, int row, int col) {
+       if (board[row][col].equals("X") || board[row][col].equals("O")) {
+           return false;
+       } else {
+           return true;
+       }
+   }
+   public static boolean checkWinX(String board[][]) {
+       if (board[0][0].equalsIgnoreCase("x") && board[0][1].equalsIgnoreCase("x") && board[0][2].equalsIgnoreCase("x") || board[1][0].equalsIgnoreCase("x") && board[1][1].equalsIgnoreCase("X") && board[1][2].equalsIgnoreCase("x") || board[2][0].equalsIgnoreCase("x") && board[2][1].equalsIgnoreCase("x") && board[2][2].equalsIgnoreCase("x")
+               || board[0][0].equalsIgnoreCase("x") && board[1][0].equalsIgnoreCase("x") && board[2][0].equalsIgnoreCase("x") || board[0][1].equalsIgnoreCase("x") && board[1][1].equalsIgnoreCase("x") && board[2][1].equalsIgnoreCase("x") || board[0][2].equalsIgnoreCase("X") && board[1][2].equalsIgnoreCase("x") && board[2][2].equalsIgnoreCase("x") ||
+               board[0][0].equalsIgnoreCase("x") && board[1][1].equalsIgnoreCase("x") && board[2][2].equalsIgnoreCase("x") || board[0][2].equalsIgnoreCase("x") && board[1][1].equalsIgnoreCase("x") && board[2][0].equalsIgnoreCase("x")){
+           System.out.println("Player X won!");
+           return true;
 
-    public static boolean placement(String[][] board, int row, int col) {
-        if (board[row][col].equals("X") || board[row][col].equals("O")) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+       }
+       return false;
+   }
+   public static boolean checkWinO(String board[][]) {
+       if (board[0][0].equalsIgnoreCase("o") && board[0][1].equalsIgnoreCase("o") && board[0][2].equalsIgnoreCase("o") || board[1][0].equalsIgnoreCase("o") && board[1][1].equalsIgnoreCase("o") && board[1][2].equalsIgnoreCase("o") || board[2][0].equalsIgnoreCase("o") && board[2][1].equalsIgnoreCase("o") && board[2][2].equalsIgnoreCase("o")
+               || board[0][0].equalsIgnoreCase("o") && board[1][0].equalsIgnoreCase("o") && board[2][0].equalsIgnoreCase("o") || board[0][1].equalsIgnoreCase("o") && board[1][1].equalsIgnoreCase("o") && board[2][1].equalsIgnoreCase("o") || board[0][2].equalsIgnoreCase("o") && board[1][2].equalsIgnoreCase("o") && board[2][2].equalsIgnoreCase("o") ||
+               board[0][0].equalsIgnoreCase("o") && board[1][1].equalsIgnoreCase("o") && board[2][2].equalsIgnoreCase("o") || board[0][2].equalsIgnoreCase("o") && board[1][1].equalsIgnoreCase("o") && board[2][0].equalsIgnoreCase("o")){
+           System.out.println("Player X won!");
+           return true;
+
+       }
+       return false;
+   }
+   public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+       int count =0;
+       String board[][] = new String[3][3];
+       System.out.println("Welcome to tic-tac-toe...do you want to be X or O");
+       String ans = input.next();
+       if (ans.equalsIgnoreCase("x")) {
+           for (int row = 0; row < 3; row++) {
+               for (int column = 0; column < 3; column++) {
+                   board[0][0] = "0,0";
+                   board[0][1] = "0,1";
+                   board[0][2] = "0,2";
+                   board[1][0] = "1,0";
+                   board[1][1] = "1,1";
+                   board[1][2] = "1,2";
+                   board[2][0] = "2,0";
+                   board[2][1] = "2,1";
+                   board[2][2] = "2,2";
+               }
+           }
+           printBoard(board);
+           for (int q = 0; q < 8; q++) {
+               System.out.println("Enter the first number of the order pair you want to move in");
+               int pos = input.nextInt();
+               System.out.println("Enter the second number of the order pair you want to move in");
+               int pos2 = input.nextInt();
+               if (!placement(board, pos, pos2)) {
+                   System.out.println("INVALID MOVE, Please pick again");
+                   System.out.println("Enter the first number of the order pair you want to move in");
+                   pos = input.nextInt();
+                   System.out.println("Enter the second number of the order pair you want to move in");
+                   pos2 = input.nextInt();
+                   for (int i = 0; i < 8; i++) {
+                       for (int j = 0; j < 8; j++) {
+                           if (pos == i && pos2 == j) {
+                               board[i][j] = "X";
+                               checkWinX(board);
+                           }
+                       }
+                   }
+                   System.out.println();
+               }
+               for (int i = 0; i < 8; i++) {
+                   for (int j = 0; j < 8; j++) {
+                       if (pos == i && pos2 == j) {
+                           board[i][j] = "X";
+                           checkWinX(board);
+                       }
+                   }
+               }
+               System.out.println();
+               Random ran = new Random();
+               int x = ran.nextInt(3);
+               int y = ran.nextInt(3);
+               if (!placement(board, x, y)) {
+                   x = ran.nextInt(3);
+                   y = ran.nextInt(3);
+                   for (int i = 0; i < 8; i++) {
+                       for (int j = 0; j < 8; j++) {
+                           if (x == i && y == j) {
+                               board[i][j] = "O";
+                               printBoard(board);
+                               checkWinX(board);
+                               System.out.println("Computer moved into position (" + x + "," + y + ")");
+                           }
+                       }
+                   }
+                   System.out.println();
+               }
+               for (int i = 0; i < 8; i++) {
+                   for (int j = 0; j < 8; j++) {
+                       if (x == i && y == j) {
+                           board[i][j] = "O";
+                           printBoard(board);
+                           System.out.println("Computer moved into position (" + x + "," + y + ")");
 
 
-    public static void main(String[] args) {
+                       }
+                   }
+               }
+                   if(checkWinX(board)){
+                       break;
+                   }
+               else if(checkWinO(board)){
+                       System.out.println("You literally just got beat by the crapiest AI ever");
+                       break;
+                   }
 
-        Scanner input = new Scanner(System.in);
-        int count =0;
-        String board[][] = new String[3][3];
-        System.out.println("Welcome to tic-tac-toe...do you want to be X or O");
-        String ans = input.next();
-        if (ans.equalsIgnoreCase("x")) {
-            for (int row = 0; row < 3; row++) {
-                for (int column = 0; column < 3; column++) {
-                    board[0][0] = "0,0";
-                    board[0][1] = "0,1";
-                    board[0][2] = "0,2";
-                    board[1][0] = "1,0";
-                    board[1][1] = "1,1";
-                    board[1][2] = "1,2";
-                    board[2][0] = "2,0";
-                    board[2][1] = "2,1";
-                    board[2][2] = "2,2";
-                }
-            }
-            printBoard(board);
-            for (int q = 0; q < 8; q++) {
-                System.out.println("Enter the first number of the order pair you want to move in");
-                int pos = input.nextInt();
-                System.out.println("Enter the second number of the order pair you want to move in");
-                int pos2 = input.nextInt();
-                if (!placement(board, pos, pos2)) {
-                    System.out.println("INVALID MOVE, Please pick again");
-                    System.out.println("Enter the first number of the order pair you want to move in");
-                    pos = input.nextInt();
-                    System.out.println("Enter the second number of the order pair you want to move in");
-                    pos2 = input.nextInt();
-                    for (int i = 0; i < 8; i++) {
-                        for (int j = 0; j < 8; j++) {
-                            if (pos == i && pos2 == j) {
-                                board[i][j] = "X";
-                                printBoard(board);
-                                count++;
-                            }
-                        }
-                    }
-                    System.out.println();
-                }
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 8; j++) {
-                        if (pos == i && pos2 == j) {
-                            board[i][j] = "X";
-                            printBoard(board);
-                            count++;
-                        }
-                    }
-                }
-                System.out.println();
-                Random ran = new Random();
-                int x = ran.nextInt(3);
-                //System.out.println("Computer's first number " + x);
-                int y = ran.nextInt(3);
-                //System.out.println("Computer's second number " + y);
-                if (!placement(board, x, y)) {
-                    x = ran.nextInt(3);
-                    //  System.out.println("Computer's first number " + x);
-                    y = ran.nextInt(3);
-                    //System.out.println("Computer's second number " + y);
-                    for (int i = 0; i < 8; i++) {
-                        for (int j = 0; j < 8; j++) {
-                            if (x == i && y == j) {
-                                board[i][j] = "O";
-                                printBoard(board);
-                                count++;
-                                System.out.println("Computer moved into position (" + x + "," + y + ")");
-                            }
-                        }
-                    }
-                    System.out.println();
-                }
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 8; j++) {
-                        if (x == i && y == j) {
-                            board[i][j] = "O";
-                            printBoard(board);
-                            System.out.println("Computer moved into position (" + x + "," + y + ")");
+           }
+       } else if (ans.equalsIgnoreCase("O")) {
+           System.out.println("Please be patient as this program is still under construction");
+       }
 
 
-                        }
-                    }
-                }
-            }
-        } else if (ans.equalsIgnoreCase("O")) {
-            System.out.println("Please be patient as this program is still under construction");
-        }
-
-
-    }
+   }
 }
+
 
